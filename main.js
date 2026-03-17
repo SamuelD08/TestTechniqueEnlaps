609cc6e0393b64102db4d2f33a2b1badbb836aa0
dejouxs <samuel.dejoux@grenoble-inp.org>
2026-03-17 18:10:27 +0100

    ajout d'un premier élément dans la scène + Ajout des lumières

--- main.js
+++ main.js
@@ -16,4 +16,30 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize(900, 800);
 document.body.append(renderer.domElement);
 
+
+
+//////////////////// CREATION DES FORMES /////////////////////
+
+// Sol
+const solGeometry = new THREE.PlaneGeometry(1, 1);
+const solMaterial = new THREE.MeshBasicMaterial({
+    color: 0x00ff00
+});
+const sol = new THREE.Mesh(solGeometry, solMaterial);
+
+// Ajouts
+scene.add(sol)
+
+//////////////////// CREATION DES Lumières /////////////////////
+// Ambiance
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+
+// Directionnel
+const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+
+// Ajouts
+scene.add(ambientLight);
+
+
+
 renderer.render(scene, camera);
